bd19c3bb06a44c0211dd16aba95c5b73e806e085
HawaiianPizza <ahmedzakariya355@gmail.com>
2019-12-13 02:22:04 -0500

    Sorry this is the REAL merge wtih kevin's code. git with visual studio is wonky

--- CIS375Project/CIS375Project/LoginScreen.Designer.cs
+++ CIS375Project/CIS375Project/LoginScreen.Designer.cs
@@ -30,82 +30,113 @@ namespace CIS375Project
         /// </summary>
         private void InitializeComponent()
         {
-               this.UserLoginButton = new System.Windows.Forms.Button();
-               this.label1 = new System.Windows.Forms.Label();
-               this.label2 = new System.Windows.Forms.Label();
-               this.UserNameBox = new System.Windows.Forms.TextBox();
-               this.PasswordBox = new System.Windows.Forms.TextBox();
-               this.SuspendLayout();
-               // 
-               // UserLoginButton
-               // 
-               this.UserLoginButton.Location = new System.Drawing.Point(158, 192);
-               this.UserLoginButton.Name = "UserLoginButton";
-               this.UserLoginButton.Size = new System.Drawing.Size(390, 118);
-               this.UserLoginButton.TabIndex = 0;
-               this.UserLoginButton.Text = "Login as User";
-               this.UserLoginButton.UseVisualStyleBackColor = true;
-               this.UserLoginButton.Click += new System.EventHandler(this.UserLoginButton_Click);
-               // 
-               // label1
-               // 
-               this.label1.AutoSize = true;
-               this.label1.Location = new System.Drawing.Point(14, 66);
-               this.label1.Name = "label1";
-               this.label1.Size = new System.Drawing.Size(83, 20);
-               this.label1.TabIndex = 2;
-               this.label1.Text = "Username";
-               // 
-               // label2
-               // 
-               this.label2.AutoSize = true;
-               this.label2.Location = new System.Drawing.Point(14, 118);
-               this.label2.Name = "label2";
-               this.label2.Size = new System.Drawing.Size(78, 20);
-               this.label2.TabIndex = 3;
-               this.label2.Text = "Password";
-               // 
-               // UserNameBox
-               // 
-               this.UserNameBox.Location = new System.Drawing.Point(158, 58);
-               this.UserNameBox.Name = "UserNameBox";
-               this.UserNameBox.Size = new System.Drawing.Size(166, 26);
-               this.UserNameBox.TabIndex = 4;
-               this.UserNameBox.TextChanged += new System.EventHandler(this.UserNameBox_TextChanged);
-               // 
-               // PasswordBox
-               // 
-               this.PasswordBox.Location = new System.Drawing.Point(158, 118);
-               this.PasswordBox.Name = "PasswordBox";
-               this.PasswordBox.Size = new System.Drawing.Size(166, 26);
-               this.PasswordBox.TabIndex = 5;
-               // 
-               // LoginScreen
-               // 
-               this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-               this.ClientSize = new System.Drawing.Size(686, 329);
-               this.Controls.Add(this.PasswordBox);
-               this.Controls.Add(this.UserNameBox);
-               this.Controls.Add(this.label2);
-               this.Controls.Add(this.label1);
-               this.Controls.Add(this.UserLoginButton);
-               this.Name = "LoginScreen";
-               this.Text = "Login";
-               this.ResumeLayout(false);
-               this.PerformLayout();
+            this.UIUserLoginButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UserNameBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.DBLoginOversight = new System.Windows.Forms.Button();
+            this.DBLoginDpartment = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UIUserLoginButton
+            // 
+            this.UIUserLoginButton.Location = new System.Drawing.Point(105, 125);
+            this.UIUserLoginButton.Margin = new System.Windows.Forms.Padding(2);
+            this.UIUserLoginButton.Name = "UIUserLoginButton";
+            this.UIUserLoginButton.Size = new System.Drawing.Size(260, 77);
+            this.UIUserLoginButton.TabIndex = 0;
+            this.UIUserLoginButton.Text = "Login as User";
+            this.UIUserLoginButton.UseVisualStyleBackColor = true;
+            this.UIUserLoginButton.Click += new System.EventHandler(this.UserLoginButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 77);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // UserNameBox
+            // 
+            this.UserNameBox.Location = new System.Drawing.Point(105, 38);
+            this.UserNameBox.Margin = new System.Windows.Forms.Padding(2);
+            this.UserNameBox.Name = "UserNameBox";
+            this.UserNameBox.Size = new System.Drawing.Size(112, 20);
+            this.UserNameBox.TabIndex = 4;
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(105, 77);
+            this.PasswordBox.Margin = new System.Windows.Forms.Padding(2);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(112, 20);
+            this.PasswordBox.TabIndex = 5;
+            // 
+            // DBLoginOversight
+            // 
+            this.DBLoginOversight.Location = new System.Drawing.Point(234, 11);
+            this.DBLoginOversight.Margin = new System.Windows.Forms.Padding(2);
+            this.DBLoginOversight.Name = "DBLoginOversight";
+            this.DBLoginOversight.Size = new System.Drawing.Size(119, 42);
+            this.DBLoginOversight.TabIndex = 6;
+            this.DBLoginOversight.Text = "DEBUG Login Oversight";
+            this.DBLoginOversight.UseVisualStyleBackColor = true;
+            this.DBLoginOversight.Click += new System.EventHandler(this.DBLoginOversight_Click);
+            // 
+            // DBLoginDpartment
+            // 
+            this.DBLoginDpartment.Location = new System.Drawing.Point(234, 77);
+            this.DBLoginDpartment.Margin = new System.Windows.Forms.Padding(2);
+            this.DBLoginDpartment.Name = "DBLoginDpartment";
+            this.DBLoginDpartment.Size = new System.Drawing.Size(119, 42);
+            this.DBLoginDpartment.TabIndex = 7;
+            this.DBLoginDpartment.Text = "DEBUG Login Departmnet";
+            this.DBLoginDpartment.UseVisualStyleBackColor = true;
+            this.DBLoginDpartment.Click += new System.EventHandler(this.DBLoginDpartment_Click);
+            // 
+            // LoginScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 214);
+            this.Controls.Add(this.DBLoginDpartment);
+            this.Controls.Add(this.DBLoginOversight);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.UserNameBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UIUserLoginButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "LoginScreen";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button UserLoginButton;
+        private System.Windows.Forms.Button UIUserLoginButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox UserNameBox;
         private System.Windows.Forms.TextBox PasswordBox;
-
-          
+        private System.Windows.Forms.Button DBLoginOversight;
+        private System.Windows.Forms.Button DBLoginDpartment;
     }
 }
 
--- CIS375Project/CIS375Project/LoginScreen.cs
+++ CIS375Project/CIS375Project/LoginScreen.cs
@@ -18,19 +18,23 @@ namespace CIS375Project
             InitializeComponent();
         }
 
-         private void LoginUserCheck(int dept)
+        private void LoginUserCheck(int dept)
         {
-               //Check in database function (IDK how the database is going to work, or even what it's going to upload. So I'll pretend it does something ehre.
-               int UserStatus = dept; //This would be int UserStatus = Database.UserCheck(user, password);
-               if (UserStatus > 1)
-               {
-                    UserStatus = 0;
-               }
+            //Check in database function (IDK how the database is going to work, or even what it's going to upload. So I'll pretend it does something ehre.
+            int UserStatus = dept; //This would be int UserStatus = Database.UserCheck(user, password);
+            if (UserStatus > 1)
+            {
+                UserStatus = 0;
+            }
+            if (UserStatus > 1)
+            {
+                UserStatus = 0;
+            }
             switch (UserStatus)
             {
                 // Can't find user in database
                 case -1:
-                    UserLoginButton.Text=("Wrong password.  Try again");
+                    UIUserLoginButton.Text = ("Wrong password.  Try again");
                     break;
                 // User is department
                 case 0:
@@ -47,21 +51,55 @@ namespace CIS375Project
                     Application.Exit();
                     break;
                 default:
-                    UserLoginButton.Text=("A critical bug has happen");
+                    UIUserLoginButton.Text = ("A critical bug has happen");
                     break;
 
             }
 
 
 
-        } 
+        }
 
         private void UserLoginButton_Click(object sender, EventArgs e)
         {
-             string Password = PasswordBox.Text;
-            string User = UserNameBox.Text;
-            LoginUserCheck(User, Password); 
+            Login c = new Login();
+            //takes in the user in put and send it to the class functions
+            c.Password = PasswordBox.Text;
+            c.Username = UserNameBox.Text;
+            bool good = c.Credentials(c);
+            //if the credentials match then it sends to the department choosing
+            if (good == true)
+            {
+                //this gets the department from the login class
+                int dept = c.GetDepartment(c);
+                LoginUserCheck(dept);
+            }
+            else
+            {
+                //sends back that it wasnt correct
+                int dept = -1;
+                LoginUserCheck(dept);
+
+            }
+
         }
 
+        private void DBLoginOversight_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            OversightScreen FormOver = new OversightScreen();
+            FormOver.ShowDialog();
+            Application.Exit();
+
+        }
+
+        private void DBLoginDpartment_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            DepartmentScreen FormDepart = new DepartmentScreen();
+            FormDepart.ShowDialog();
+            Application.Exit();
+
+        }
     }
 }
